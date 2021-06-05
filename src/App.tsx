109a2efe246bfc2eef679e2455ab2c69bca88181
Naohito Sadoshima <nsadoshima@gmail.com>
2021-06-05 13:52:17 -0400

    fixed mobile view

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ interface AppProps {}
 const StyledApp = styled.div`
   display: grid;
   min-height: 100vh;
-  grid-template-columns: 1fr 500px 1fr;
+  grid-template-columns: 1fr auto 1fr;
   grid-template-rows: 200px 100px auto auto;
   grid-template-areas:
     'header title header2'
@@ -123,8 +123,8 @@ function App({}: AppProps) {
     <StyledApp className="bg-gray-100 antialiased text-gray-800">
       <div className="py-6 sm:py-12" style={{ gridArea: 'title' }}>
         <div className="relative py-3 sm:max-w-xl sm:mx-auto">
-          <div className="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 bg-red-300 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
-          <div className="relative bg-white shadow-lg sm:rounded-3xl">
+          <div className="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 bg-red-300 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 rounded-3xl"></div>
+          <div className="relative bg-white shadow-lg rounded-3xl">
             <h1 className="font-sans font-semibold text-center w-full text-3xl text-gray-500 p-10">
               <span className="text-blue-400">Rewind</span> and{' '}
               <span className="text-red-400">Be Kind</span>
